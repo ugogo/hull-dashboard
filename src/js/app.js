9113973ae5b029a9b889a3d579d427f9a04400fe
onaliugo <contact@onaliugo.fr>
2014-09-16 16:00:21 +0200

    Encapsule fetchSettings into a function, display enhancements

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,12 @@
 
-var opts = {
-  orgurl: "https://835b18df.hullapp.io",
-  appid: "540ef0175f6f92e55b001366",
-};
-
-Stull.init(opts, function(){
-  // on init
-
-  // display a notification
-  Notify.show('success', 'Hull initialized successfully');
+var fetchSettings = (function(){
 
   // for each settings container
   $('.js-settings-container').each(function(i, el){
     var $this = $(el);
 
     // create a variable with options
-    // related to the container
+    // related to the current container
     var opts = {
       pattern: $this.attr('data-pattern'),
       $model: $('<fieldset> <label></label> <input /> </fieldset>'),
@@ -23,10 +14,25 @@ Stull.init(opts, function(){
     };
 
     // fetch and display settings
-    // related to the container
+    // related to the current container
     Settings.fetch(opts, function(json){
       Settings.display();
+      $('.js-settings-section').removeClass('none');
     });
-  });
 
+    // do stuff
+  });
+});
+var mainOpts = {
+  orgurl: "https://835b18df.hullapp.io",
+  appid: "540ef0175f6f92e55b001366",
+};
+
+Stull.init(mainOpts, function(){
+  // on init
+  // display a notification
+  Notify.show('success', 'Hull initialized successfully');
+
+  // then, fetch settings
+  fetchSettings();
 });
